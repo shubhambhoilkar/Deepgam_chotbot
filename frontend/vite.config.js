f64302ff18441e5d4d3417e4e5fa1827c8aecfbf
Shubham Bhoilkar <91750972+shubhambhoilkar@users.noreply.github.com>
2025-06-17 10:49:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const backendURL = process.env.BACKENDURL_URL || 'HTTP://localhost:9900'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/ws': {
-        target: 'ws://localhost:9900',
+        target: backendURL.replace(/^http/,'ws'),
         ws: true
       }
     }
